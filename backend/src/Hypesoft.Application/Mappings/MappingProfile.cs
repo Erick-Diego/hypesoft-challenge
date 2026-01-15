51fd2dddff813df62cdc07ff055f4826c1f61133
Erick <erick.dc.batista@gmail.com>
2026-01-15 18:31:18 -0300

    feat(application): add automapper mapping profile for products and categories

---
+++ backend/src/Hypesoft.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,49 @@
+using AutoMapper;
+using Hypesoft.Application.DTOs;
+using Hypesoft.Domain.Entities;
+
+namespace Hypesoft.Application.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        // Product Mappings
+        CreateMap<Product, ProductDto>()
+            .ForMember(dest => dest.IsLowStock, opt => opt.MapFrom(src => src.IsLowStock))
+            .ForMember(dest => dest.IsOutOfStock, opt => opt.MapFrom(src => src.IsOutOfStock))
+            .ForMember(dest => dest.TotalStockValue, opt => opt.MapFrom(src => src.TotalStockValue));
+
+        CreateMap<CreateProductDto, Product>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.IsActive, opt => opt.Ignore())
+            .ForMember(dest => dest.Sku, opt => opt.Ignore());
+
+        CreateMap<UpdateProductDto, Product>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.IsActive, opt => opt.Ignore())
+            .ForMember(dest => dest.StockQuantity, opt => opt.Ignore())
+            .ForMember(dest => dest.Sku, opt => opt.Ignore());
+
+        // Category Mappings
+        CreateMap<Category, CategoryDto>();
+
+        CreateMap<CreateCategoryDto, Category>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.IsActive, opt => opt.Ignore())
+            .ForMember(dest => dest.Products, opt => opt.Ignore());
+
+        CreateMap<UpdateCategoryDto, Category>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.IsActive, opt => opt.Ignore())
+            .ForMember(dest => dest.Products, opt => opt.Ignore());
+    }
+}
